3699d72a0df8850e5b1f5170f5d4619d479ca260
Daniel Lyck <d9@live.dk>
2019-11-10 00:27:32 +0100

    Forside-komponent oprettet og routing opsat.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "./App.css";
 
+// Import Routing
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// Import Components
+import TextArea from "./components/textArea/TextArea";
+
 function App() {
-  return <h1>TSN Client Boilerplate</h1>;
+  return (
+    <Router>
+      <h1>Header her</h1>
+      <Switch>
+        <Route path="/" exact component={TextArea} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/textArea/TextArea.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./TextArea.css";
+
+function TextArea() {
+  return <h1 id="test">Forside tekst her</h1>;
+}
+
+export default TextArea;
