fa2af106c8a37be6a1d5016a688bef9768e4154f
Mathias <xuniqh@gmail.com>
2019-11-11 21:08:33 +0100

    Uploaded 'dummy-funktion' til signout

--- src/components/textArea/TextArea.js
+++ src/components/textArea/TextArea.js
@@ -1,8 +1,36 @@
-import React from "react";
-import "./TextArea.css";
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import './TextArea.css';
+
+// Me query
+export const ME = gql`
+  {
+    me {
+      _id
+      firstName
+      lastName
+      mail
+    }
+  }
+`;
 
 function TextArea() {
-  return <h1 id="test">Forside tekst her</h1>;
+  const { client } = useQuery(ME);
+
+  const Logout = () => {
+    localStorage.clear();
+    client.resetStore();
+    window.location = '/';
+  };
+
+  return (
+    <Link to="#" onClick={Logout} className="text-dark d-block mb-1">
+      Sign Out
+    </Link>
+    // <h1 id="test">Forside tekst her</h1>;
+  );
 }
 
 export default TextArea;
