b543ed612ff01c4898da689ce7b06bf02ac21983
unknown <xuniqh@gmail.com>
2019-11-11 13:46:42 +0100

    Oprettet login og querie mutation

--- src/components/loginForm/LoginForm.js
+++ src/components/loginForm/LoginForm.js
@@ -1,5 +1,30 @@
-import React from "react";
-import "./LoginForm.css";
+import React from 'react';
+import { gql } from 'apollo-boost';
+import './LoginForm.css';
+
+// Me query
+export const ME = gql`
+  {
+    me {
+      _id
+      firstName
+      lastName
+      mail
+    }
+  }
+`;
+// Login Mutation
+export const LOGIN = gql`
+  mutation login($mail: String!, $password: String!) {
+    login(mail: $mail, password: $password) {
+      _id
+      firstName
+      lastName
+      mail
+      token
+    }
+  }
+`;
 
 function LoginForm() {
   return <h1>Login form her</h1>;
