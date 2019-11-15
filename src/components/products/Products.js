e1de5395cf632f51de1630e52bdc49a764d2aa18
Daniel Lyck <d9@live.dk>
2019-11-15 11:15:10 +0100

    Produktkomponentet opsat og klar til kamp.

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import './Products.css';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
 
+// Komponent, der renderer alle produkter
 function Products() {
-  return (
-    <React.Fragment>
-      <h3>Liste over alle produkter</h3>
-      <p>Den kommer, den kommer. Også listen.</p>
-    </React.Fragment>
-  );
+  // Definér query til at hente alle produkter
+  const GET_CUSTOMERS = gql`
+    {
+      getCustomers {
+        _id
+        name
+        vatNo
+      }
+    }
+  `;
+
+  // Anvend query
+  const { loading, error, data } = useQuery(GET_CUSTOMERS);
+
+  if (loading)
+    return (
+      <tr>
+        <td>Loading...</td>
+      </tr>
+    );
+  if (error)
+    return (
+      <tr>
+        <td>Error!</td>
+      </tr>
+    );
+  return data.getCustomers.map(({ _id, name, vatNo }) => (
+    <tr key={_id}>
+      <td>
+        <Link to={`/dashboard/customers/${_id}`}>{name}</Link>
+      </td>
+      <td>
+        <Link to={`/dashboard/customers/${_id}`}>{vatNo}</Link>
+      </td>
+    </tr>
+  ));
 }
 
 export default Products;
