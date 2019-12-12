50e72337e40bb5a5e39910c2efe0b7fba6fb879d
Daniel <31951102+dani832m@users.noreply.github.com>
2019-12-12 14:23:49 +0100

    Update EditSubCategory.js

--- src/components/categories/EditSubCategory.js
+++ src/components/categories/EditSubCategory.js
@@ -22,21 +22,22 @@ function EditSubCategory() {
       }
     }
   `;
+  
   const UPDATE_SUB_CATEGORY_BY_ID = gql`
-  mutation {
-    updateSubCategoryById(
-      _id: "${inputId}"
-      input: {
-        name: "${inputName}"
-        categoryId: "${inputCategoryId}"
-        imagePath: "${inputImagePath}"
+    mutation {
+      updateSubCategoryById(
+        _id: "${inputId}"
+        input: {
+          name: "${inputName}"
+          categoryId: "${inputCategoryId}"
+          imagePath: "${inputImagePath}"
+        }
+      ){
+        name
+        categoryId
+        imagePath
       }
-    ){
-      name
-      categoryId
-      imagePath
     }
-  }
   `;
 
   const { loading, error, data } = useQuery(GET_SUB_CATEGORIES);
@@ -46,7 +47,7 @@ function EditSubCategory() {
   if (error) return <p>Error...</p>;
 
   return data.getSubCategories.map(subCategory => {
-    const { _id, name, categoryId, imagePath } = subCategory; //Destructoring
+    const { _id, name, categoryId, imagePath } = subCategory; // Destructoring
 
     const handleSubmit = event => {
       event.preventDefault();
