45f4ceff86e52ada19a4d4c1066eacc39a663fba
Daniel <31951102+dani832m@users.noreply.github.com>
2019-12-13 15:16:53 +0100

    React Fragment redundant og dermed fjernet.

--- src/components/editTextarea/EditTextareaFooter.js
+++ src/components/editTextarea/EditTextareaFooter.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import './EditTextarea.css';
 
-// Komponent, der indeholder teksten under WYSIWYG boksen.
+// Komponent, der indeholder teksten under WYSIWYG-boksen.
 function EditTextareaFooterText() {
   return (
-    <React.Fragment>
-      <p className="footerTextStyles">
-        Såfremt at der ændres i teksten, vil den ændrede tekst automatisk blive
-        gemt og vist på forsiden.
-      </p>
-    </React.Fragment>
+    <p className="footerTextStyles">
+      Såfremt at der ændres i teksten, vil den ændrede tekst automatisk blive
+      gemt og vist på forsiden.
+    </p>
   );
 }
 
