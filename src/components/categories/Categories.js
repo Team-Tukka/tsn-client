acd63e65ca966488df171799cdde542d4aca3841
Daniel Lyck <d9@live.dk>
2019-12-17 11:53:56 +0100

    Collapse på kategorier og splittegninger.

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import AddNewCategory from './AddNewCategory';
 import AddNewSubCategory from './AddNewSubCategory';
 import EditCategory from './EditCategory';
@@ -6,7 +7,7 @@ import EditSubCategory from './EditSubCategory';
 import './Categories.css';
 
 // Importér Reactstrap komponenter
-import { Col, Row } from 'reactstrap';
+import { Col, Row, Collapse } from 'reactstrap';
 
 /*
   Parent Component, der indeholder Sub Components, som giver
@@ -14,33 +15,72 @@ import { Col, Row } from 'reactstrap';
   ændre splittegninger (underkategorier)
 */
 function Categories() {
+  // States med React Hooks, der håndterer toggle-status
+  const [catToggleIsOpen, setCatToggleIsOpen] = useState(false);
+  const [subCatToggleIsOpen, setSubCatToggleIsOpen] = useState(false);
+
+  // Toggle-funktioner
+  const categoriesToggle = () => setCatToggleIsOpen(!catToggleIsOpen);
+  const subCategoriesToggle = () => setSubCatToggleIsOpen(!subCatToggleIsOpen);
+
   return (
     <Row>
       <Col>
         <Row>
           <Col>
-            <h3 className="mb-3">Kategorier</h3>
-            <EditCategory />
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <h3 className="mb-3">Splittegninger</h3>
-            <EditSubCategory />
-          </Col>
-        </Row>
-      </Col>
-      <Col>
-        <Row>
-          <Col>
-            <h3 className="mb-3">Tilføj kategori</h3>
+            <h4 className="mb-3">Tilføj kategori</h4>
             <AddNewCategory />
           </Col>
         </Row>
         <Row>
           <Col>
-            <h3 className="mb-3">Tilføj splittegning</h3>
+            <h4 className="my-3">Tilføj splittegning</h4>
             <AddNewSubCategory />
+          </Col>
+        </Row>
+      </Col>
+      <Col>
+        <Row>
+          <Col className="mb-2">
+            <div className="flexElements">
+              <h4 className="mb-3">Kategorier</h4>
+              <Link
+                onClick={categoriesToggle}
+                className="showHideTextStyles linkStyles mb-3"
+              >
+                Vis/skjul
+              </Link>
+            </div>
+            {catToggleIsOpen === false && (
+              <span>
+                Tryk på "Vis/skjul" for at vise og skjule listen med kategorier.
+              </span>
+            )}
+            <Collapse isOpen={catToggleIsOpen} className="fadeIn">
+              <EditCategory />
+            </Collapse>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="flexElements">
+              <h4 className="my-3">Splittegninger</h4>
+              <Link
+                onClick={subCategoriesToggle}
+                className="showHideTextStyles linkStyles my-3"
+              >
+                Vis/skjul
+              </Link>
+            </div>
+            {subCatToggleIsOpen === false && (
+              <span>
+                Tryk på "Vis/skjul" for at vise og skjule listen med
+                splittegninger.
+              </span>
+            )}
+            <Collapse isOpen={subCatToggleIsOpen} className="fadeIn">
+              <EditSubCategory />
+            </Collapse>
           </Col>
         </Row>
       </Col>
