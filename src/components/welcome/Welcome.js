922186db759659c4d4ffa7279cf342b18e2f8575
Daniel Lyck <d9@live.dk>
2020-01-07 11:13:21 +0100

    Lille rettelse.

--- src/components/welcome/Welcome.js
+++ src/components/welcome/Welcome.js
@@ -7,8 +7,8 @@ function Welcome() {
     <React.Fragment>
       <h3 className="mb-2">Velkommen</h3>
       <p>
-        Du er nu logget ind i systemet. I navigationsbaren herover har du nu
-        fået en række administrative muligheder baseret på din rolle.
+        Du er nu logget ind i systemet. I navigationsbaren herover har du fået
+        en række administrative muligheder baseret på din rolle.
       </p>
     </React.Fragment>
   );
